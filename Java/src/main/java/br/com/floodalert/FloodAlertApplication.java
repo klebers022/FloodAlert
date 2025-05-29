cb5f0f2d350e3762df7fe85829ce2344a515f616
LucasRainha <lucas.rainha.domingues@gmail.com>
2025-05-29 12:16:28 -0300

    criação do projeto

---
+++ Java/src/main/java/br/com/floodalert/FloodAlertApplication.java
@@ -0,0 +1,13 @@
+package br.com.floodalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloodAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloodAlertApplication.class, args);
+	}
+
+}
---
+++ Java/src/test/java/br/com/floodalert/FloodAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.floodalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloodAlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
